ec2f0c3fbaea12ff7824763b7e7b73586bf243ab
vikaspuri.goswami <vgoswami@navigatingcancer.com>
2022-10-31 17:55:53 +0530

    Removed code and moved build in root folder

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.blog
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/index.dart
+++
@@ -1,3 +0,0 @@
-// Export pages
-export 'signup/signup_widget.dart' show SignupWidget;
-export 'home_page/home_page_widget.dart' show HomePageWidget;
